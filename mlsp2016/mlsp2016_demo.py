90b7c8ddefeedfc1640ef49a31743798953b8ff4
Jakub <jacobnzw@gmail.com>
2016-05-16 12:25:38 +0300

    Added comparison of GP fits w/ andw/o derivatives.

--- mlsp2016/mlsp2016_demo.py
+++ mlsp2016/mlsp2016_demo.py
@@ -2,6 +2,7 @@ from __future__ import division
 import numpy as np
 import numpy.linalg as la
 import pandas as pd
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from matplotlib.lines import Line2D
@@ -11,6 +12,7 @@ from transforms.quad import MonteCarlo, Unscented, GaussHermite, SphericalRadial
 from transforms.bayesquad import GPQuad, GPQuadDerRBF
 from models.ungm import UNGM
 from scipy.stats import norm
+from scipy.linalg import cho_factor, cho_solve
 from numba import jit
 
 
@@ -352,21 +354,220 @@ def gpq_sos_demo():
     row_labels = [t.__class__.__name__ for t in transforms]
     col_labels = [str(d) for d in dims]
     sos_table = pd.DataFrame(sos_data, index=row_labels * 2, columns=col_labels)
-    return sos_table, ivar_data
+    ivar_table = pd.DataFrame(ivar_data[1:, :], index=['GPQ', 'GPQ+D'], columns=col_labels)
+    return sos_table, ivar_table, ivar_data
 
 
-pd.set_option('display.float_format', '{:.2f}'.format)
-sos, ivar = gpq_sos_demo()
-# save_table(sos, 'sum_of_squares.tex')
-print sos
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-ax.set_xlim(0, 26)
-ax.set_xticks([1, 5, 10, 25])
-ax.grid(which='major')
-plt.semilogy(ivar[0, :], ivar[1, :], 'ko', ivar[0, :], ivar[2, :], 'k^', ms=8)
-plt.show()
+def maha(x, y, V=None):
+    """Pair-wise Mahalanobis distance of rows of x and y with given weight matrix V.
 
+    :param x: (n, d) matrix of row vectors
+    :param y: (n, d) matrix of row vectors
+    :param V: weight matrix (d, d), if V=None, V=eye(d) is used
+    :return:
+    """
+    if V is None:
+        V = np.eye(x.shape[1])
+    x2V = np.sum(x.dot(V) * x, 1)
+    y2V = np.sum(y.dot(V) * y, 1)
+    return (x2V[:, na] + y2V[:, na].T) - 2 * x.dot(V).dot(y.T)
+
+
+def kern_rbf_der(xs, x, alpha=1.0, el=1.0, which_der=None):
+    """RBF kernel w/ derivatives."""
+    x, xs = np.atleast_2d(x), np.atleast_2d(xs)
+    D, N = x.shape
+    Ds, Ns = xs.shape
+    assert Ds == D
+    which_der = np.arange(N) if which_der is None else which_der
+    Nd = len(which_der)  # points w/ derivative observations
+    # extract hypers
+    # alpha, el, jitter = hypers['sig_var'], hypers['lengthscale'], hypers['noise_var']
+    iLam = np.diag(el ** -1 * np.ones(D))
+    iiLam = np.diag(el ** -2 * np.ones(D))
+
+    x = iLam.dot(x)  # sqrt(Lambda^-1) * X
+    xs = iLam.dot(xs)
+    Kff = np.exp(2 * np.log(alpha) - 0.5 * maha(xs.T, x.T))  # cov(f(xi), f(xj))
+    x = iLam.dot(x)  # Lambda^-1 * X
+    xs = iLam.dot(xs)
+    XmX = xs[..., na] - x[:, na, :]  # pair-wise differences
+    Kfd = np.zeros((Ns, D * Nd))  # cov(f(xi), df(xj))
+    Kdd = np.zeros((D * Nd, D * Nd))  # cov(df(xi), df(xj))
+    for i in range(Ns):
+        for j in range(Nd):
+            jstart, jend = j * D, j * D + D
+            j_d = which_der[j]
+            Kfd[i, jstart:jend] = Kff[i, j_d] * XmX[:, i, j_d]
+    for i in range(Nd):
+        for j in range(Nd):
+            istart, iend = i * D, i * D + D
+            jstart, jend = j * D, j * D + D
+            i_d, j_d = which_der[i], which_der[j]  # indices of points with derivatives
+            Kdd[istart:iend, jstart:jend] = Kff[i_d, j_d] * (iiLam - np.outer(XmX[:, i_d, j_d], XmX[:, i_d, j_d]))
+    return Kff, Kfd, Kdd  # np.vstack((np.hstack((Kff, Kfd)), np.hstack((Kfd.T, Kdd))))
+
+
+def gp_fit_demo(f, pars, xrng=(-1, 1, 50), save_figs=False, alpha=1.0, el=1.0):
+    xs = np.linspace(*xrng)  # test set
+    fx = np.apply_along_axis(f, 0, xs[na, :], pars).squeeze()
+    xtr = np.sqrt(3) * np.array([-1, 1], dtype=float)  # train set
+    ytr = np.apply_along_axis(f, 0, xtr[na, :], pars).squeeze()  # function observations + np.random.randn(xtr.shape[0])
+    dtr = np.apply_along_axis(f, 0, xtr[na, :], pars, dx=True).squeeze()  # derivative observations
+    y = np.hstack((ytr, dtr))
+    m, n = len(xs), len(xtr)  # train and test points
+    jitter = 1e-8
+    # evaluate kernel matrices
+    kss, kfd, kdd = kern_rbf_der(xs, xs, alpha=alpha, el=el)
+    kff, kfd, kdd = kern_rbf_der(xs, xtr, alpha=alpha, el=el)
+    kfy = np.hstack((kff, kfd))
+    Kff, Kfd, Kdd = kern_rbf_der(xtr, xtr, alpha=alpha, el=el)
+    K = np.vstack((np.hstack((Kff, Kfd)), np.hstack((Kfd.T, Kdd))))
+    # GP fit w/ function values only
+    kff_iK = cho_solve(cho_factor(Kff + jitter * np.eye(n)), kff.T).T
+    gp_mean = kff_iK.dot(ytr)
+    gp_var = np.diag(kss - kff_iK.dot(kff.T))
+    gp_std = np.sqrt(gp_var)
+    # GP fit w/ functionn values and derivatives
+    kfy_iK = cho_solve(cho_factor(K + jitter * np.eye(n + n * 1)), kfy.T).T  # kx.dot(inv(K))
+    gp_mean_d = kfy_iK.dot(y)
+    gp_var_d = np.diag(kss - kfy_iK.dot(kfy.T))
+    gp_std_d = np.sqrt(gp_var_d)
+
+    # setup plotting
+    fmin, fmax, fp2p = np.min(fx), np.max(fx), np.ptp(fx)
+    axis_limits = [-3, 3, fmin - 0.2 * fp2p, fmax + 0.2 * fp2p]
+    tick_settings = {'which': 'both', 'bottom': 'off', 'top': 'off', 'left': 'off', 'right': 'off', 'labelleft': 'off',
+                     'labelbottom': 'off'}
+    # use tex to render text in the figure
+    mpl.rc('text', usetex=True)
+    # use lmodern font package which is also used in the paper
+    mpl.rc('text.latex', preamble=['\usepackage{lmodern}'])
+    # sans serif font for figure, size 10pt
+    mpl.rc('font', family='sans-serif', size=10)
+    plt.style.use('seaborn-paper')
+    # set figure width to fit the column width of the article
+    pti = 1.0 / 72.0  # 1 inch = 72 points
+    fig_width_pt = 244  # obtained from latex using \the\columnwidth
+    golden_mean = (np.sqrt(5.0) - 1.0) / 2.0
+    fig_w = fig_width_pt * pti * 1.0
+    fig_h = fig_w * golden_mean
+    plt.figure(figsize=(fig_w, fig_h))
+
+    # # plot ordinary GP regression fit
+    # plt.subplot(211)
+    # plt.axis(axis_limits)
+    # plt.tick_params(**tick_settings)
+    # plt.title('GP regression')
+    # plt.plot(xs, fx, 'r--', label='true')
+    # plt.plot(xtr, ytr, 'ko', ms=8, label='observed fcn values')
+    # plt.plot(xs, gp_mean, 'k-', lw=2, label='GP mean')
+    # plt.fill_between(xs, gp_mean - 2 * gp_std, gp_mean + 2 * gp_std, color='k', alpha=0.15)
+    # # plot GP regression fit w/ derivative observations
+    # plt.subplot(212)
+    # plt.axis(axis_limits)
+    # plt.tick_params(**tick_settings)
+    # plt.title('GP regression with gradient observations')
+    # plt.plot(xs, fx, 'r--', label='true')
+    # plt.plot(xtr, ytr, 'ko', ms=8, label='observed fcn values')
+    # plt.plot(xs, gp_mean_d, 'k-', lw=2, label='GP mean')
+    # plt.fill_between(xs, gp_mean_d - 2 * gp_std_d, gp_mean_d + 2 * gp_std_d, color='k', alpha=0.15)
+    # # plot line segments to indicate derivative observations
+    # h = 0.15
+    # for i in range(len(dtr)):
+    #     x0, x1 = xtr[i] - h, xtr[i] + h
+    #     y0 = dtr[i] * (x0 - xtr[i]) + ytr[i]
+    #     y1 = dtr[i] * (x1 - xtr[i]) + ytr[i]
+    #     plt.gca().add_line(Line2D([x0, x1], [y0, y1], linewidth=6, color='k'))
+    # plt.tight_layout()
+    # if save_figs:
+    #     plt.savefig('{}_gpr_grad_compar.pdf'.format(f.__name__), format='pdf')
+    # else:
+    #     plt.show()
+
+    # two figure version
+    scale = 0.5
+    fig_width_pt = 244 / 2
+    fig_w = fig_width_pt * pti
+    fig_h = fig_w * golden_mean * 1
+    # plot ordinary GP regression fit
+    plt.figure(figsize=(fig_w, fig_h))
+    plt.axis(axis_limits)
+    plt.tick_params(**tick_settings)
+    plt.plot(xs, fx, 'r--', label='true')
+    plt.plot(xtr, ytr, 'ko', ms=8, label='observed fcn values')
+    plt.plot(xs, gp_mean, 'k-', lw=2, label='GP mean')
+    plt.fill_between(xs, gp_mean - 2 * gp_std, gp_mean + 2 * gp_std, color='k', alpha=0.15)
+    plt.tight_layout(pad=0.5)
+    if save_figs:
+        plt.savefig('{}_gpr_fcn_obs_small.pdf'.format(f.__name__), format='pdf')
+    else:
+        plt.show()
+    # plot GP regression fit w/ derivative observations
+    plt.figure(figsize=(fig_w, fig_h))
+    plt.axis(axis_limits)
+    plt.tick_params(**tick_settings)
+    plt.plot(xs, fx, 'r--', label='true')
+    plt.plot(xtr, ytr, 'ko', ms=8, label='observed fcn values')
+    plt.plot(xs, gp_mean_d, 'k-', lw=2, label='GP mean')
+    plt.fill_between(xs, gp_mean_d - 2 * gp_std_d, gp_mean_d + 2 * gp_std_d, color='k', alpha=0.15)
+    # plot line segments to indicate derivative observations
+    h = 0.15
+    for i in range(len(dtr)):
+        x0, x1 = xtr[i] - h, xtr[i] + h
+        y0 = dtr[i] * (x0 - xtr[i]) + ytr[i]
+        y1 = dtr[i] * (x1 - xtr[i]) + ytr[i]
+        plt.gca().add_line(Line2D([x0, x1], [y0, y1], linewidth=6, color='k'))
+    plt.tight_layout(pad=0.5)
+    if save_figs:
+        plt.savefig('{}_gpr_grad_obs_small.pdf'.format(f.__name__), format='pdf')
+    else:
+        plt.show()
+
+        # integral variances
+        # d = 1
+        # ut_pts = Unscented.unit_sigma_points(d)
+        # # f = UNGM().dyn_eval
+        # mean = np.zeros(d)
+        # cov = np.eye(d)
+        # gpq = GPQuad(d, unit_sp=ut_pts, hypers={'sig_var': alpha, 'lengthscale': el * np.ones(d), 'noise_var': 1e-8})
+        # gpqd = GPQuadDerRBF(d, unit_sp=ut_pts,
+        #                     hypers={'sig_var': alpha, 'lengthscale': el * np.ones(d), 'noise_var': 1e-8},
+        #                     which_der=np.arange(ut_pts.shape[1]))
+        # mct = MonteCarlo(d, n=2e4)
+        # mean_gpq, cov_gpq, cc_gpq = gpq.apply(f, mean, cov, np.atleast_1d(1.0))
+        # mean_gpqd, cov_gpqd, cc_gpqd = gpqd.apply(f, mean, cov, np.atleast_1d(1.0))
+        # mean_mc, cov_mc, cc_mc = mct.apply(f, mean, cov, np.atleast_1d(1.0))
+        #
+        # xmin_gpq = norm.ppf(0.0001, loc=mean_gpq, scale=gpq.integral_var)
+        # xmax_gpq = norm.ppf(0.9999, loc=mean_gpq, scale=gpq.integral_var)
+        # xmin_gpqd = norm.ppf(0.0001, loc=mean_gpqd, scale=gpqd.integral_var)
+        # xmax_gpqd = norm.ppf(0.9999, loc=mean_gpqd, scale=gpqd.integral_var)
+        # xgpq = np.linspace(xmin_gpq, xmax_gpq, 500)
+        # ygpq = norm.pdf(xgpq, loc=mean_gpq, scale=gpq.integral_var)
+        # xgpqd = np.linspace(xmin_gpqd, xmax_gpqd, 500)
+        # ygpqd = norm.pdf(xgpqd, loc=mean_gpqd, scale=gpqd.integral_var)
+        # #
+        # plt.figure(figsize=(fig_w, fig_h))
+        # plt.axis([np.min([xmin_gpq, xmin_gpqd]), np.max([xmax_gpq, xmax_gpqd]), 0, np.max(ygpqd) + 0.2 * np.ptp(ygpqd)])
+        # plt.tick_params(**tick_settings)
+        # plt.plot(xgpq, ygpq, 'k-.', lw=2)
+        # plt.plot(xgpqd, ygpqd, 'k-', lw=2)
+        # plt.gca().add_line(Line2D([mean_mc, mean_mc], [0, 10], color='r', ls='--', lw=2))
+        # plt.tight_layout(pad=0.5)
+        # if save_figs:
+        #     plt.savefig('{}_gpq_int_var.pdf'.format(f.__name__), format='pdf')
+        # else:
+        #     plt.show()
+
+
+sos_table, ivar_table, ivar = gpq_sos_demo()
+# save_table(sos_table, 'sum_of_squares.tex')
+pd.set_option('display.float_format', '{:.2e}'.format)
+save_table(ivar_table, 'sos_gpq_int_var.tex')
+
+# gp_fit_demo(UNGM().dyn_eval, [1], xrng=(-3, 3, 50), alpha=10.0, el=0.7)
+gp_fit_demo(sos, None, xrng=(-3, 3, 50), alpha=1.0, el=10.0, save_figs=True)
 # gpq_int_var_demo()
 
 # fig = plot_func(rss, 2, n=100)
