34bf04ab62fcba8b3c2b020b23472fed357a57db
Jakub Pruher <jacobnzw@gmail.com>
2016-09-08 15:35:16 +0200

    Resetting after forward pass is a must. Total position RMSE in time for both filters. Still not getting the same results as with the reentry demo in tracking.py.

--- paper_code/gpq_tracking.py
+++ paper_code/gpq_tracking.py
@@ -11,14 +11,15 @@ from models.tracking import ReentryRadar as ReentryRadarModel
 def reentry_gpq_demo():
     # Generate reference trajectory by ODE integration
     sys = ReentryRadar()
-    mc_sims = 10
-    # x = sys.simulate_trajectory(method='euler', dt=0.5, duration=100, mc_sims=mc_sims)
+    mc_sims = 25
+    # x = sys.simulate_trajectory(method='rk4', dt=0.5, duration=200, mc_sims=mc_sims)
     # x_ref = x.mean(axis=2)
     # y = sys.simulate_measurements(x_ref, mc_per_step=mc_sims)
 
     # Initialize filters
     ssm = ReentryRadarModel()
     x, y = ssm.simulate(steps=750, mc_sims=10)
+    x_ref = x.mean(axis=2)
     hdyn = {'alpha': 1.0, 'el': 25*np.ones(5,)}
     hobs = {'alpha': 1.0, 'el': [25.0, 25.0, 1e4, 1e4, 1e4]}
     alg = (GPQKalman(ssm, 'rbf', 'ut', hdyn, hobs),
@@ -30,6 +31,7 @@ def reentry_gpq_demo():
     for imc in range(mc_sims):
         for ia, a in enumerate(alg):
             mean[..., imc, ia], cov[..., imc, ia] = a.forward_pass(y[..., imc])
+            a.reset()
 
     # Plots
     plt.figure()
@@ -54,13 +56,15 @@ def reentry_gpq_demo():
     for k in range(steps):
         for imc in range(mc_sims):
             for a in range(num_alg):
-                error2[:, k, imc, a] = squared_error(x[:, k, imc], mean[:, k, imc, a])
-    pos_rmse_vs_time = np.sqrt((error2[:2, ...] ** 2).sum(axis=0)).mean(axis=1)
+                error2[:, k, imc, a] = squared_error(x_ref[:, k], mean[:, k, imc, a])
+    pos_rmse_vs_time = np.sqrt((error2[:2, ...]).sum(axis=0)).mean(axis=1)
     plt.subplot(g[:, 2:])
     plt.plot(pos_rmse_vs_time[:, 0], label='gpqkf', color='g')
     plt.plot(pos_rmse_vs_time[:, 1], label='ukf', color='orange')
     plt.legend()
     plt.show()
+
+    print(pos_rmse_vs_time.mean(axis=0))
 
 if __name__ == '__main__':
     reentry_gpq_demo()
