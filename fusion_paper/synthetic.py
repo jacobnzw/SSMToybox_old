13a30d3e420909b7804759dc0e5e686119561fe3
Jakub Pruher <jacobnzw@gmail.com>
2017-03-29 15:17:27 +0200

    Refactoring: moved ssmod.py to the root, renamed to sssim.py (state-space simulation).

--- fusion_paper/synthetic.py
+++ fusion_paper/synthetic.py
@@ -8,7 +8,7 @@ from mtran import FullySymmetricStudent, Taylor1stOrder
 from ssinf import StudentInference
 from ssinf import UnscentedKalman, TPQStudent
 from ssmod import StateSpaceModel, StudentStateSpaceModel
-from system.datagen import System
+from sssim import System
 from utils import log_cred_ratio, mse_matrix, bigauss_mixture, multivariate_t
 
 
