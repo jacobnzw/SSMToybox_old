ca078f7fd3e812b95ded0aae31b42b51e18552a6
Jakub Pruher <jacobnzw@gmail.com>
2017-04-13 11:36:34 +0200

    Random rotatations for higher dimensional points added, some method parameters changed, other test functions added.

--- kernel_fitting.py
+++ kernel_fitting.py
@@ -1,15 +1,17 @@
 import numpy as np
+import scipy as sp
 from numpy import newaxis as na
 from transforms.bqmodel import GaussianProcess
-import scipy as sp
-
+from scipy.optimize import minimize
 
 # true function
 # f = lambda x: x**2
-f = lambda x: 2*x + 0.5*x**2 + np.sin(x**2) - np.cos(2*x**2)
-f = lambda x: 25*x / (1 + x**2)
+# f = lambda x: 2*x + 0.5*x**2 + np.sin(x**2) - np.cos(2*x**2)
+# f = lambda x: 25*x / (1 + x**2)
+f = lambda x: sp.special.expit(np.sin((x+4)**2)+np.cos(x+4))
+f = lambda x: np.cos(x)+np.sin(0.5*x)
 
-model = GaussianProcess(1, np.array([[1.0, 1.0]]))
+model = GaussianProcess(1, np.array([[1.0, 1.0]]), points='gh', point_par={'degree': 3})
 
 # sigma-point template
 x = model.points
@@ -18,19 +20,31 @@ num_dim, num_pts = x.shape
 
 # samples used for non-parametric kernel matrix estimation
 num_samples = 100
-sample_std = 1
+sample_std = 0.0001
 
 # plot model
-xtest = np.linspace(-3, 3, 100)[na, :]
+xtest = np.linspace(-5, 5, 100)[na, :]
 ytest = np.apply_along_axis(f, 1, xtest)
-model.plot_model(xtest, y, np.array([[1.0, 0.1]]), ytest)
+theta_0 = np.array([[1.0, 0.1]])
+model.plot_model(xtest, y, theta_0, ytest)
 
 
 # generate pertubed templates
 def perturb_template(x, samples=10, std=1.0):
     num_dim, num_pts = x.shape
+
+    # random translation
     delta = np.random.multivariate_normal(np.zeros((num_dim, )), std*np.eye(num_dim), size=samples).T
-    return x[..., na] + delta[:, na, :]
+    res = x[..., na] + delta[:, na, :]
+
+    if num_dim > 1:
+        # random rotation in n-D
+        A = np.random.rand(num_dim, num_dim, num_samples)
+        for s in range(num_samples):
+            Q, R = np.linalg.qr(A[..., s])
+            res[..., s] = Q.dot(res[..., s])
+
+    return res
 
 
 # non-parametric kernel matrix estimate
@@ -46,7 +60,8 @@ def estimate_kernel_mat(y):
 # kernel fitting objective
 def kernel_obj(log_theta, x, K_true):
     K_theta = model.kernel.eval(np.exp(log_theta), x)
-    return np.linalg.norm(K_true - K_theta)
+    return np.linalg.norm(K_true - K_theta, ord=np.inf)
+    # return np.linalg.norm(K_true - K_theta, ord='fro')
 
 
 X = perturb_template(x, samples=num_samples, std=sample_std)
@@ -57,11 +72,19 @@ for i in range(num_samples):
 K_true = estimate_kernel_mat(Y)
 
 # minimize matrix norm to find optimal kernel parameters
-from scipy.optimize import minimize
-log_theta_0 = np.log(np.array([[1, 1]], dtype=float))
+log_theta_0 = np.log(np.array([[1.0, 1.0]]))
 opt = minimize(kernel_obj, log_theta_0, args=(x, K_true), jac=False, method='BFGS')
+theta_star = np.exp(opt.x)
 
-print('Optimal kernel parameters (samples={0:d}): {1:}'.format(num_samples, np.exp(opt.x)))
+# optimization with multiple restarts
+# opt_list = []
+# for i in range(10):
+#     log_theta_0 = np.random.multivariate_normal(np.zeros((2,)), 0.1*np.eye(2)).T
+#     opt = minimize(kernel_obj, log_theta_0, args=(x, K_true), jac=False, method='BFGS')
+#     opt_list.append(opt)
+# theta_star = np.exp(opt_list[np.argmin([ol.fun for ol in opt_list])].x)
+
+print('Optimal kernel parameters (samples={0:d}): {1:}'.format(num_samples, theta_star))
 
 # plot the GP fit
-model.plot_model(xtest, y, np.exp(opt.x), ytest)
+model.plot_model(xtest, y, theta_star, ytest)
